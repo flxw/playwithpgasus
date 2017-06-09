758b3b0d706fce1d22298d96982da33d3e353061
Felix Wolff <felix.wolff2@student.hpi.de>
2017-06-09 19:33:43 +0200

    Fix CMAKE and tasking test to run as many tasks as there are nodes

--- tasking_test.cpp
+++ tasking_test.cpp
@@ -1,90 +1,39 @@
-#include <cstring>
-#include <cstdio>
-#include <cmath>
-#include <string>
-#include <sstream>
-
 #include <vector>
 #include <list>
 #include <iostream>
 
-#include "tasking/tasking.hpp"
-#include "timer.hpp"
-#include "test_helper.h"
+#include <base/node.hpp>
+#include <tasking/tasking.hpp>
 
-using numa::TaskRef;
-using numa::TriggerableRef;
+//using numa::TaskRef;
+//using numa::TriggerableRef;
+using namespace std;
 
-inline float tediousCalc() {	return 0.f; }
-
-void usage(const char *name) {
-	printf("Usage: %s taskcount spawner\n", name);
-	exit(0);
-}
-
-
-int main (int argc, char const* argv[])
-{
-	if (argc < 3) usage(argv[0]);
-
-	testing::initialize();
-
-	// get task count
-	int count = atoi(argv[1]);
-	int count2 = count/2;
-	int spawner = atoi(argv[2]);
-
-	printf("Main: %d+%d tasks, %d spawner\n",
-		count, count2, spawner);
-
-	count /= spawner;
-	count2 /= spawner;
-
-	std::list<TriggerableRef> spawnerTasks;
-	for (int i = 0; i < spawner; i++) {
-		spawnerTasks.push_back(numa::async<void>( [=] () {
-			printf("Spawner[%d] start\n", i);
-
-			// run some simple tests
-			std::vector<TriggerableRef> tasks;
-			std::list<TriggerableRef> waitTasks;
-
-			for (int i = 0; i < count; i++) {
-				tasks.push_back(numa::async<void>( [i] () {
-					Timer<int> t(true);
-					tediousCalc();
-
-					// int total = t.stop_get();
-//					printf("Task[%d] done: %d.%03ds\n", i, total/1000, total%1000);
-				}, 0));
-			}
-
-			for (int i = 0; i < count2; i++) {
-				waitTasks.push_back(numa::async<void>( [=,&tasks] () {
-					Timer<int> t(true);
-					numa::wait(tasks[2*i].get());
-					/*int w1 =*/ t.stop_get_start();
-					numa::wait(tasks[2*i+1].get());
-					/*int w2 =*/ t.stop_get_start();
-					tediousCalc();
-					/*int tt =*/ t.stop_get_start();
-
-					//printf("Task2[%d] done: w1=%d.%03ds, w2=%d.%03ds t=%d.%03ds\n",
-					//	i, w1/1000, w1%1000, w2/1000, w2%1000, tt/1000, tt%1000);
-				}, 1));
-			}
-
-			printf("Spawner[%d] wait\n", i);
-
-			numa::wait(waitTasks);
-
-			printf("Spawner[%d] done\n", i);
-		}, 0));
+ostream& operator<<(ostream &os, const vector<numa::CpuId> &cl) {
+	for (int i = 0; i < cl.size(); ++i) {
+		os << (int)cl[i] << " ";
 	}
 
-	printf("[Main] wait\n");
-	numa::wait(spawnerTasks);
-	printf("[Main] done\n");
+	return os;
+}
+
+void printTaskNode() {
+	cout << "Task running on node PHY" << numa::Node::curr().physicalId()
+			 << " LOG" << numa::Node::curr().logicalId() << endl;
+}
+
+int main (int argc, char const* argv[]) {
+	int priority = 0;
+	int core = 0;
+	list<numa::TriggerableRef> taskList;
+
+	numa::NodeList nodeList = numa::NodeList::allNodes();
+
+	for (int i = 0; i < nodeList.size(); ++i) {
+		taskList.push_back(numa::async<void>(printTaskNode, priority, nodeList[i]));
+	}
+
+	numa::wait(taskList);
 
 	return 0;
 }
